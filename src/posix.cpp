5bb4c0df7544861a3ddfd7923c36ddd4f4972a6d
Alexander Saers <alexander.saers@gmail.com>
2021-02-13 17:44:04 +0100

    Created an example were readers are given priority.

--- src/posix.cpp
+++ src/posix.cpp
@@ -4,12 +4,44 @@
 #include <iostream>
 #include <shared_mutex>
 
+
+// shared_mutex_write_prio is a shared_mutex
+// that gives writer priority. This prevents starvation 
+// of write locks when multiple readers are polling
+// the resource.
+class shared_mutex_write_prio : public std::shared_mutex
+{
+    public:
+    shared_mutex_write_prio()
+    {
+#ifdef __USE_GNU
+
+        // Lets do this the ugly way.
+        // We need to setup 
+        auto m = static_cast<pthread_rwlock_t *>(native_handle());
+        // Destroy already initialized rwlock
+        pthread_rwlock_destroy(m);
+        pthread_rwlockattr_t attr;
+        pthread_rwlockattr_setkind_np(&attr, PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP);
+        // Create a new one
+        pthread_rwlock_init(m, &attr);
+#else
+    #error Must be using GNU for this
+#endif
+    }
+
+    ~shared_mutex_write_prio() = default;
+
+    shared_mutex_write_prio(const shared_mutex_write_prio&) = delete;
+    shared_mutex_write_prio& operator=(const shared_mutex_write_prio&) = delete;
+};
+
 class Bar
 {
     public:
     Bar() = default;
 
-    std::shared_timed_mutex m_mutex;
+    shared_mutex_write_prio m_mutex;
     unsigned int m_num_clients{0};
     bool m_running{true};
 
@@ -19,7 +51,7 @@ void shared_poller(Bar& f, int n)
 {
     while(f.m_running)
     {
-        std::shared_lock<std::shared_timed_mutex> lock(f.m_mutex);
+        std::shared_lock<std::shared_mutex> lock(f.m_mutex);
         std::cout << "Poller " << n << " has lock" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
         lock.unlock();
@@ -33,7 +65,7 @@ void shared_releaser(Bar& f)
     std::this_thread::sleep_for(std::chrono::seconds(2));
     std::cout << "Releaser trying to take mutex" << std::endl;
 
-    std::unique_lock<std::shared_timed_mutex> lock(f.m_mutex);
+    std::unique_lock<std::shared_mutex> lock(f.m_mutex);
     std::cout << "Releaser stop running" << std::endl;
     f.m_running = false;
 }
